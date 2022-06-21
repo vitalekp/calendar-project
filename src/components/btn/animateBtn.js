05a8fc3d3ddb44ac6a694b7b21f195d0866bdeb9
vitalekp <vitalekpushkarov@gmail.com>
2022-06-21 12:31:28 +0300

    upd

---
+++ src/components/btn/animateBtn.js
@@ -0,0 +1,12 @@
+const animateBtn = (e) => {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+export default animateBtn;
---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,25 @@
+import React, { Component, useState } from "react";
+
+import Navigation from "./../navigation/Navigation";
+import Week from "../week/Week";
+import Sidebar from "../sidebar/Sidebar";
+import events from "../../gateway/events";
+
+import "./calendar.scss";
+
+const Calendar = ({ weekDates }) => {
+  const [event, setEvents] = useState(events);
+  return (
+    <section className="calendar">
+      <Navigation weekDates={weekDates} />
+      <div className="calendar__body">
+        <div className="calendar__week-container">
+          <Sidebar />
+          <Week weekDates={weekDates} events={event} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Calendar;
